e409155b6bffa248b8d5bb530767552b9378d864
rrbixler <rrbixler@sandiego.edu>
2019-06-03 14:20:02 -0400

    Commit 2 -setup config

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://shielded-cove-86358.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
